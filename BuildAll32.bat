d1c176df6ab84c8dd6c7c1d55e91a22b2624c848
Gergul <mail0668@gmail.com>
2018-11-10 12:06:03 +0800

    BAT可点击直接编译

--- BuildAll32.bat
+++ BuildAll32.bat
@@ -1,3 +1,6 @@
+@For /F "Skip=1" %%i in ('Wmic Logicaldisk Where "DriveType=3" Get Name') Do call "%%i\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat" %2
+set VisualStudioVersion=14.0
+
 pushd "%CD%"
 
 cd /d %~dp0
@@ -7,3 +10,5 @@ cd /d %~dp0
 call curl-7.60.0\BuildCurl32.bat
 
 popd
+
+pause
--- BuildAll64.bat
+++ BuildAll64.bat
@@ -1,3 +1,6 @@
+@For /F "Skip=1" %%i in ('Wmic Logicaldisk Where "DriveType=3" Get Name') Do call "%%i\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64_x86\vcvarsamd64_x86.bat" %2
+set VisualStudioVersion=14.0
+
 pushd "%CD%"
 
 cd /d %~dp0
@@ -7,3 +10,5 @@ cd /d %~dp0
 call curl-7.60.0\BuildCurl64.bat
 
 popd
+
+pause
