a87081eb7692e2055f5754a3a5d83858d8064f6a
mensong <coolcn@qq.com>
2024-03-29 10:30:01 +0800

    ansi

--- curl-7.60.0/winbuild/BuildCurl_x86.bat
+++ curl-7.60.0/winbuild/BuildCurl_x86.bat
@@ -3,8 +3,8 @@ cd /d "%~dp0"
 
 ::set RTLIBCFG=static or <null>
 ::set DEBUG=yes or no
-::set openssl_dir=opensslçš„è·¯å¾„
-::set zlib_dir=zlibçš„è·¯å¾„
+::set openssl_dir=opensslµÄÂ·¾¶
+::set zlib_dir=zlibµÄÂ·¾¶
 
 nmake /f Makefile.vc mode=static RTLIBCFG=%RTLIBCFG% MACHINE=x86 ENABLE_WINSSL=no ENABLE_IDN=no ENABLE_SSPI=no DEBUG=%DEBUG% WITH_SSL=static WITH_ZLIB=static SSL_PATH=%openssl_dir% ZLIB_PATH=%zlib_dir%
 
