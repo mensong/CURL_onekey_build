eca7f7e304f970a6c6e4372e663c4e9906075e1e
mensong <coolcn@qq.com>
2024-05-08 14:56:40 +0800

    调整zlib编译顺序

--- deps/zlib-1.2.11/BuildZLib.bat
+++ deps/zlib-1.2.11/BuildZLib.bat
@@ -4,10 +4,10 @@
 ::set platform=x64
 
 rem ±‡“Îzlib
-set zlibstat_vcxproj=%~dp0\contrib\vstudio\%vc_ver%\zlibstat.vcxproj
-devenv.exe "%zlibstat_vcxproj%" /build "%config%|%platform%" /project "%zlibstat_vcxproj%"
 set zlibvc_vcxproj=%~dp0\contrib\vstudio\%vc_ver%\zlibvc.vcxproj
 devenv.exe "%zlibvc_vcxproj%" /build "%config%|%platform%" /project "%zlibvc_vcxproj%"
+set zlibstat_vcxproj=%~dp0\contrib\vstudio\%vc_ver%\zlibstat.vcxproj
+devenv.exe "%zlibstat_vcxproj%" /build "%config%|%platform%" /project "%zlibstat_vcxproj%"
 
 set outDir=%~dp0\out\%vc_ver%\%platform%\%config%
 mkdir "%outDir%\include"
